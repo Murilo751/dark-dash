0f1dc7205f3be261e272f2ad475ea34789e391bc
Murilo751 <murilo.matos2005@gmail.com>
2024-11-12 19:28:51 -0300

    ADD: ligacao com o banco, e comunicacao entre telas

--- app/src/main/java/com/example/dark_dash/CreateTaskActivity.java
+++ app/src/main/java/com/example/dark_dash/CreateTaskActivity.java
@@ -1,8 +1,12 @@
 package com.example.dark_dash;
 
+import android.content.Intent;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,11 +14,17 @@ public class CreateTaskActivity extends AppCompatActivity {
     private EditText taskTitle;
     private EditText taskDescription;
     private Button saveButton;
+    private SQLiteDatabase connection;
+    private DataBaseHelper db_helper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
+
+        //criando conexão com o banco
+        createConection();
 
         taskTitle = findViewById(R.id.taskTitle);
         taskDescription = findViewById(R.id.taskDescription);
@@ -26,7 +36,22 @@ public class CreateTaskActivity extends AppCompatActivity {
     private void saveTask() {
         String title = taskTitle.getText().toString();
         String description = taskDescription.getText().toString();
-
+        Intent voltar_main = new Intent(CreateTaskActivity.this, MainActivity.class);
+        startActivity(voltar_main);
         // Aqui você pode salvar os dados, seja em um banco de dados local ou enviando para uma API
+        TaskDAO taskDAO = new TaskDAO(connection);
+        Task task = new Task(title, description);
+        taskDAO.insert(task);
+    }
+
+    private void createConection(){
+        try{
+            Toast.makeText(this,R.string.sucess_conection,Toast.LENGTH_LONG).show();
+            db_helper = new DataBaseHelper(this.getApplicationContext());
+            connection = db_helper.getWritableDatabase();
+            Toast.makeText(this,R.string.sucess_conection,Toast.LENGTH_LONG).show();
+        }catch(SQLException e){
+            Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show();
+        }
     }
 }
